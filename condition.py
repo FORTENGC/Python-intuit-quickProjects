1e66103466ed4b7c20bd64e8934c36bc6e686048
FORTENGC <47381501+FORTENGC@users.noreply.github.com>
2021-01-28 09:55:08 +0200

    Add files via upload

---
+++ condition.py
@@ -0,0 +1,9 @@
+for counter in range(1, 11):
+    print("its my room stay outside")
+
+while True:
+    answer = input("Are you bored yet? (y/n)")
+    if answer == "y":
+        print("How rude!")
+    break
+    
